1697e4f710bb8c22beef463dc5c0adf0278063ca
kon3gor <bboymishka888@gmail.com>
2020-05-03 13:58:25 +0300

    register layout

--- app/src/main/java/org/sk/hopelife/MainActivity.kt
+++ app/src/main/java/org/sk/hopelife/MainActivity.kt
@@ -1,12 +1,21 @@
 package org.sk.hopelife
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.reg).setOnClickListener {
+            Log.i("fuck", "clicked")
+            var intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
